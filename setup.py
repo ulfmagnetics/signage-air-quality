e85738c2124ed70a1a3605f9b6d2dc92b877f0e4
John Berry <ulfmagnetics@gmail.com>
2020-01-06 21:18:46 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name="signage-air-quality",
+    version='0.0.1',
+    author='John Berry',
+    author_email='ulfmagnetics@gmail.com',
+    packages=find_packages(),
+    url='http://github.com/ulfmagnetics/signage-air-quality',
+    license='LICENSE',
+    description='Air quality classes for the signage system',
+    install_requires=['textwrap']
+)
